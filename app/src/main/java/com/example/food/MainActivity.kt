f5f5d92d93af07cabc80d088a3c5c940a97b3df5
K1wilson <wwaihwa@gmail.com>
2022-08-29 11:14:38 +0300

    food

--- app/src/main/java/com/example/food/MainActivity.kt
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+
     }
 }
