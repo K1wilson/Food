5ddce6dbaad57c92bfc4ca28dbed662926dffbac
K1wilson <wwaihwa@gmail.com>
2022-09-20 20:15:39 +0300

    1.2foodapp

--- app/src/main/java/com/example/food/MainActivity.kt
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+     val bottomnav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = Navigation.findNavController(this,R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomnav,navController)
 
     }
 }
