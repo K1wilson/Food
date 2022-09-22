ded408c80cb1d97c35a75e7564c6cf7324ee6af4
K1wilson <wwaihwa@gmail.com>
2022-09-22 19:44:24 +0300

    1.3foodapp

---
+++ app/src/main/java/com/example/food/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+    }
+}
